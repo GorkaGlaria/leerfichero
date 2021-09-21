ceb8c0583fb0abab4f375fafd5cc1888e1119068
GorkaGlaria <91153140+GorkaGlaria@users.noreply.github.com>
2021-09-21 20:13:30 +0200

    Add files via upload

---
+++ leertexto/leertexto/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace leertexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cargar = new System.Windows.Forms.Button();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.apellido = new System.Windows.Forms.TextBox();
+            this.Ciudad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listadoagenda = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.listadoagenda)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cargar
+            // 
+            this.cargar.Location = new System.Drawing.Point(713, 83);
+            this.cargar.Name = "cargar";
+            this.cargar.Size = new System.Drawing.Size(75, 23);
+            this.cargar.TabIndex = 0;
+            this.cargar.Text = "Cargar";
+            this.cargar.UseVisualStyleBackColor = true;
+            this.cargar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(68, 30);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(100, 20);
+            this.nombre.TabIndex = 1;
+            this.nombre.TextChanged += new System.EventHandler(this.nombre_TextChanged);
+            // 
+            // apellido
+            // 
+            this.apellido.Location = new System.Drawing.Point(299, 31);
+            this.apellido.Name = "apellido";
+            this.apellido.Size = new System.Drawing.Size(100, 20);
+            this.apellido.TabIndex = 3;
+            this.apellido.TextChanged += new System.EventHandler(this.apellido_TextChanged);
+            // 
+            // Ciudad
+            // 
+            this.Ciudad.Location = new System.Drawing.Point(535, 31);
+            this.Ciudad.Name = "Ciudad";
+            this.Ciudad.Size = new System.Drawing.Size(100, 20);
+            this.Ciudad.TabIndex = 4;
+            this.Ciudad.TextChanged += new System.EventHandler(this.Ciudad_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nombre :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(238, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Apellidos :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(481, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ciudad :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listadoagenda
+            // 
+            this.listadoagenda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.listadoagenda.Location = new System.Drawing.Point(15, 122);
+            this.listadoagenda.Name = "listadoagenda";
+            this.listadoagenda.Size = new System.Drawing.Size(773, 217);
+            this.listadoagenda.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listadoagenda);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Ciudad);
+            this.Controls.Add(this.apellido);
+            this.Controls.Add(this.nombre);
+            this.Controls.Add(this.cargar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.listadoagenda)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cargar;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.TextBox apellido;
+        private System.Windows.Forms.TextBox Ciudad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView listadoagenda;
+    }
+}
+
---
+++ leertexto/leertexto/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace leertexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Text files|*.txt" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamReader read = new StreamReader(ofd.FileName))
+                    {
+                        string[] columnnames = { "Nombre", "Ciudad", "Teléfono" };
+                            //read.ReadLine().Split('|');
+                        DataTable dt = new DataTable();
+                        foreach (string c in columnnames)
+                        {
+                            dt.Columns.Add(c);
+                        }
+                        string newline;
+                        while ((newline = read.ReadLine()) != null)
+                        {
+                            DataRow dr = dt.NewRow();
+                            string[] values = newline.Split('|');
+                            for (int i = 0; i < values.Length; i++)
+                            {
+                                dr[i] = values[i];
+                            }
+                          
+                                dt.Rows.Add(dr);
+                            
+                        }
+                        read.Close();
+                        listadoagenda.DataSource = dt;
+                    }
+                }
+            }
+        }
+
+      
+
+        private void nombre_TextChanged(object sender, EventArgs e)
+
+        {
+            if (listadoagenda.Rows.Count > 0)
+            {
+                (listadoagenda.DataSource as DataTable).DefaultView.RowFilter = string.Format("Nombre LIKE '{0}%'", nombre.Text);
+            }
+            else
+            {
+                MessageBox.Show( "Carga un txt");
+            }
+        }
+
+        private void apellido_TextChanged(object sender, EventArgs e)
+        {
+            if (listadoagenda.Rows.Count > 0)
+            {
+                (listadoagenda.DataSource as DataTable).DefaultView.RowFilter = string.Format("Nombre LIKE '%{0}%'", apellido.Text);
+            }
+            else
+            {
+                MessageBox.Show( "Carga un txt");
+            }
+}
+
+        private void Ciudad_TextChanged(object sender, EventArgs e)
+        {
+            if (listadoagenda.Rows.Count > 0)
+            {
+                (listadoagenda.DataSource as DataTable).DefaultView.RowFilter = string.Format("Ciudad LIKE '%{0}%'", Ciudad.Text);
+            }
+            else
+            {
+                MessageBox.Show("Carga un txt");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RecursiveClearTextBoxes(this.Controls);
+        }
+
+        private void RecursiveClearTextBoxes(Control.ControlCollection cc)
+
+        {
+
+            foreach (Control ctrl in cc)
+
+            {
+
+                TextBox tb = ctrl as TextBox;
+
+                if (tb != null)
+
+                    tb.Clear();
+
+                else
+
+                    RecursiveClearTextBoxes(ctrl.Controls);
+
+            }
+
+        }
+    }
+}
